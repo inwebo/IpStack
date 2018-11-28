5c162af2c67421c3db7800baf28bc27155057361
inwebo <gh_kvrg1hir>
2018-11-28 21:49:41 +0100

    Add enum

---
+++ Model/ContinentCodesEnum.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Inwebo\Component\IpStack\Model;
+
+class ContinentCodesEnum
+{
+    /** @var string  */
+    const AF = 'Africa';
+    /** @var string  */
+    const AS = 'Asia';
+    /** @var string  */
+    const EU = 'Europe';
+    /** @var string  */
+    const NA = 'North America';
+    /** @var string  */
+    const OC = 'Oceania';
+    /** @var string  */
+    const SA = 'South America';
+    /** @var string  */
+    const AN = 'Antarctica';
+}
---
+++ Model/ResponseLanguageEnum.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Inwebo\Component\IpStack\Model;
+
+
+class ResponseLanguageEnum
+{
+    /** @var string  */
+    const en = 'English/US';
+    /** @var string  */
+    const de = 'German';
+    /** @var string  */
+    const es = 'Spanish';
+    /** @var string  */
+    const fr = 'French';
+    /** @var string  */
+    const ja = 'Japanese';
+    /** @var string Api attribut is pt-br */
+    const ptbr = 'Portugues (Brazil)';
+    /** @var string  */
+    const ru = 'Russian';
+    /** @var string  */
+    const zh = 'Chinese';
+}
